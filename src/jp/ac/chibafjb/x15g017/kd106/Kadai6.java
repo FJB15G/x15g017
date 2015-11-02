bdd26980c85835f5e9a33ecde0ee7ce5397de1d2
unknown <x15g017@PC504N033.chiba-fjb.ac.jp>
2015-11-02 15:11:22 +0900

    課題６

---
+++ src/jp/ac/chibafjb/x15g017/kd106/Kadai6.java
@@ -0,0 +1,16 @@
+package jp.ac.chibafjb.x15g017.kd106;
+
+public class Kadai6 {
+
+	public static void main(String[] args) {
+		System.out.println("     @    ");
+		System.out.println("    @@@   ");
+		System.out.println("@@@@@@@@@@@");
+		System.out.println("  @@@@@@@");
+		System.out.println(" @@@@@@@@");
+		System.out.println(" @       @");
+		
+
+	}
+
+}
