3c80aee3f2984ced5bddd1908186648befd4b566
unknown <x15g017@PC504N033.chiba-fjb.ac.jp>
2015-11-02 15:00:49 +0900

    課題５

---
+++ src/jp/ac/chibafjb/x15g017/kd105/Kadai5.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g017.kd105;
+
+public class Kadai5 {
+
+	public static void main(String[] args) {
+		System.out.println("氏名:山田花子");
+		System.out.println("趣味:読書");
+		System.out.println("好きな動物:猫");
+
+	}
+
+}
