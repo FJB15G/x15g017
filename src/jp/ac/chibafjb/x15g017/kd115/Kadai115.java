ef8726eb1b2848e4bd8c16246a758a72e8ab56df
unknown <x15g017@PC504N033.chiba-fjb.ac.jp>
2015-11-09 14:14:51 +0900

    発展問題115

---
+++ src/jp/ac/chibafjb/x15g017/kd115/Kadai115.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.x15g017.kd115;
+
+public class Kadai115 {
+
+	public static void main(String[] args) {
+		for(int i = 0;i<2;i++)
+			System.out.println("あいうえおあいうえおあいうえお");
+		for(int j = 0;j<2;j++)
+			System.out.println("かきくけこかきくけこかきくけこ");
+		for(int n = 0;n<2;n++)
+			System.out.println("させすせそさしすせそさしすせそ");
+	}
+
+}
