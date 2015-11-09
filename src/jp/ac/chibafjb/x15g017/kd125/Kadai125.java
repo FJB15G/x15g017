bc214b89e70c9b862601fdbb525a1b0ee8d28f68
unknown <x15g017@PC504N033.chiba-fjb.ac.jp>
2015-11-09 14:41:27 +0900

    課題125

---
+++ src/jp/ac/chibafjb/x15g017/kd125/Kadai125.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g017.kd125;
+
+public class Kadai125 {
+
+	public static void main(String[] args) {
+		for(int i = 10;i<=30;i= i+10){
+			System.out.print(i);
+			System.out.println("＊");
+		}	
+	}
+
+}
