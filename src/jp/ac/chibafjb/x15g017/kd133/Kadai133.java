0af2483a8225a61f490d6a71507978b7202ee1c7
unknown <x15g017@PC504N033.chiba-fjb.ac.jp>
2015-11-09 15:00:46 +0900

    課題133

---
+++ src/jp/ac/chibafjb/x15g017/kd133/Kadai133.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g017.kd133;
+
+public class Kadai133 {
+
+	public static void main(String[] args) {
+		int x =5;
+		int y =10;
+		int z = x + y;
+		System.out.println("x+y="+z);
+	}
+
+}
