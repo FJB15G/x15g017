911bf3cb52ab2e38573603df4375cdfd71960be9
unknown <x15g017@PC504N033.chiba-fjb.ac.jp>
2015-11-09 13:57:02 +0900

    課題112

---
+++ src/jp/ac/chibafjb/x15g017/kd112/Kadai112.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g017.kd112;
+
+public class Kadai112 {
+
+	public static void main(String[] args) {
+		for(int i = 0;i<100;i++)
+			System.out.println("船橋情報ビジネス専門学校");
+	}
+
+}
