dd7deade7b6b284df8119b570cbb193e29f80266
unknown <x15g017@PC504N033.chiba-fjb.ac.jp>
2015-11-09 14:27:47 +0900

    発展問題116

---
+++ src/jp/ac/chibafjb/x15g017/kd116/Kadai116.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g017.kd116;
+
+public class Kadai116 {
+
+	public static void main(String[] args) {
+		for(int i = 0;i<2;i++){
+			System.out.println("あいうえおあいうえおあいうえお");
+			System.out.println("かきくけこかきくけこかきくけこ");
+			System.out.println("さしすせそさしすせそさしすせそ");
+		}
+	}
+
+}
