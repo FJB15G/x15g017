0234e3187847201f0c61860ed25ab5e5f3d17250
unknown <x15g017@PC504N033.chiba-fjb.ac.jp>
2015-11-16 14:10:21 +0900

    課題141

---
+++ src/jp/ac/chibafjb/x15g017/kd141/Kadai141.java
@@ -0,0 +1,15 @@
+package jp.ac.chibafjb.x15g017.kd141;
+
+import java.util.Scanner;
+
+public class Kadai141 {
+
+	public static void main(String[] args) {
+		Scanner sca = new Scanner(System.in);
+		System.out.println("整数を一つ入力してください　-->");
+		String num = sca.next();
+		System.out.println("あなたの入力した数は"+num+"です");
+		sca.close();
+	}
+
+}
