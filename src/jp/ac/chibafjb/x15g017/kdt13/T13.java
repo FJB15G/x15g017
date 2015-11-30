82d58f01a6003439849788a33372d26d9eea5d9b
unknown <x15g017@PC504N033.chiba-fjb.ac.jp>
2015-11-30 14:29:00 +0900

    課題T13

---
+++ src/jp/ac/chibafjb/x15g017/kdt13/T13.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g017.kdt13;
+
+public class T13 {
+
+	public static void main(String[] args) {
+		int a = 11;
+		for(int i = 0;i<5;i++){
+			System.out.println("35j0"+a);
+			a=a+10;
+		}
+	}
+
+}
