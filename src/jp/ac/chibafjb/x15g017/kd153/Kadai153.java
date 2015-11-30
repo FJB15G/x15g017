2fce972d68b3427dbe4f7dd7cfaee6ff5570603b
unknown <x15g017@PC504N033.chiba-fjb.ac.jp>
2015-11-30 14:13:58 +0900

    課題153

---
+++ src/jp/ac/chibafjb/x15g017/kd153/Kadai153.java
@@ -0,0 +1,19 @@
+package jp.ac.chibafjb.x15g017.kd153;
+
+import java.util.Scanner;
+
+public class Kadai153 {
+
+	public static void main(String[] args) {
+		Scanner yomu = new Scanner(System.in);
+		System.out.print("半径ｒ　-->");
+		float a = yomu.nextFloat();
+		yomu.close();
+		double b = 2 * 3.14 * a;
+		double c = a * a * 3.14;
+		System.out.println("円周　＝"+ Math.round((b*100)/100));
+		System.out.println("面積　＝"+ Math.round((c * 100)/100));
+		
+	}
+
+}
